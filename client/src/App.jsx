e8344e252bcacc76a13fa594fb7baf066bd7725c
pinkovay <pedros.pinkovay@gmail.com>
2024-01-15 12:53:41 -0300

    Função de H1 atualizando conforme ação do usuário

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,21 +5,33 @@ import Join from './components/Join/JoinScript'
 import Chat from './components/Chat/ChatScript'
 
 function App() {
+  // estes state's estão sendo criados aqui
   const [chatVisibilty, setChatVisibility] = useState(false)
   const [socket, setSocket] = useState(null)
 
+  const [chatName, setChatName] = useState("Login")
+
   return (
     <>
+
+      <div className="boxUserName">
+        <h1 className="UserName">{chatName}</h1>
+      </div>
+
       <div className="App">
         {/* condição ternária */}
 
         {
 
-          chatVisibilty ? <Chat socket={socket} /> : <Join setSocket={setSocket} setChatVisibility={setChatVisibility} />
+          // A expressão condicional ternária (condition ? expr1 : expr2) avalia chatVisibility. Se chatVisibility for verdadeiro, o componente <Chat /> será renderizado; caso contrário, o componente <Join /> será renderizado.
+
+          chatVisibilty ? <Chat socket={socket} /> : <Join setSocket={setSocket} setChatVisibility={setChatVisibility} setChatName={setChatName} />
 
         }
 
       </div>
+
+     
     </>
   )
 }
--- client/src/components/Join/JoinScript.jsx
+++ client/src/components/Join/JoinScript.jsx
@@ -2,17 +2,22 @@ import React, {useRef} from 'react';
 import io from 'socket.io-client';
 import style from './Join.module.css'
 
-const Join = ({setChatVisibility, setSocket}) => {
+
+
+const Join = ({setChatVisibility, setSocket, setChatName}) => {
 
     const userNameRef = useRef() // I need to study more about useRef
 
     const handleSubmit = async () =>{
         const username = userNameRef.current.value
-        if(!username.trim()) return // checking if the username is not empty
+        if(!username.trim()) return(window.alert("The name can't be empty")) // checking if the username is not empty
         const socket = await io.connect('http://localhost:3001') // connecting to the backend
         socket.emit('set_username', username ) // event being passed to the backend
         setSocket(socket)
         setChatVisibility(true) // Changing Chat visibility to true
+
+        
+        setChatName(`${username}'s Chat`); // Atualizando o nome do chat com o nome do usuário
     }
 
     return (
@@ -24,6 +29,6 @@ const Join = ({setChatVisibility, setSocket}) => {
             </div>
         </div>
     );
-}
+} 
 
 export default Join;
